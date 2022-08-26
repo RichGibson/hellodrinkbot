98b058eb768e532f14f612cefa49a4b82b405f20
Rich Gibson <rich.gibson@gmail.com>
2022-08-26 08:28:49 -0700

    button_ukr_wip.py works more or less by calling Bartendro. There is an apparent Bartendro quirk/bug.

--- software/roulette/README.md
+++ software/roulette/README.md
@@ -1,5 +1,23 @@
 # Ukrainian Roulette
 
+Aug 26, 2022
+
+Strip out the need for Bartendro. We have four pumps, we don't need all 
+of that code. Especially since I don't understand it.
+
+Aug 25, 2022
+
+New buttons are installed, and work. And I modified Bartendro a bit. And 
+now if you press button 2 and then button 1 it is fine, but if you 
+go the other way bartendro stops the second pump when the first one
+finishes dispensing.
+
+Also, the case is beautiful. The Burroughs-Apple montage is amazing.
+
+And I now understand why Christine liked the name. It is not 'yay
+Ukraine' it is 'we hate Russia, so it is like Freedom Fries' or 
+'Victory Cabbage.'  Just reflexive erasure of the enemy and disfavored
+
 Aug 6, 2022
 Based on an idea of Spencer's at the 'Drinkbot Hackathon and Networking'
 event May 12th, 2018
@@ -33,6 +51,15 @@ Running Hello Drinkbot image, but a custom bit of python.
 ## Decoration
 
 Thinking of a picture of William Burroughs with an apple on his head on
-the front. If you know, you know.
+the front. If you know, you know. I printed an assortment of pictures, and of
+apples at various sizes, and Christine ran with it. Yay.
 
-Painted with Ukranian colors on the parts without the picture.
+The top is painted with Ukranian colors.  Christine explained that this was funny
+in the same way that 'Freedom Fries' are funny. We have a geopolitical bruhaha (disregarding
+the legitimacy of the tiff) and we need to rename things that originate in the 'other'
+nation.
+
+Freedom Fries. Liberty cabbage in place of saurkraut. etc.
+
+
+
---
+++ software/roulette/button_ukr_wip.py
@@ -0,0 +1,95 @@
+#PDX-FileCopyrightText: 2022 Kattni Rembor for Adafruit Industries
+# SPDX-License-Identifier: MIT
+"""Arcade QT example that pulses the button LED on button press"""
+import time
+import board
+import digitalio
+from adafruit_seesaw.seesaw import Seesaw
+from adafruit_seesaw.digitalio import DigitalIO
+from adafruit_seesaw.pwmout import PWMOut
+
+from threading import Thread
+import urllib.request
+
+# For most boards.
+i2c = board.I2C()
+
+# Todo: This sometimes fails on start up
+# RuntimeError: Seesaw hardware ID returned (0xc3) is not correct! Expected 0x55 or 0x87. Please check your wiring.
+arcade_qt = Seesaw(i2c, addr=0x3A)
+
+# initialize buttons[] and leds[]
+# Button pins in order (1, 2, 3, 4)
+button_pins = (18, 19, 20, 2)
+buttons = []
+for button_pin in button_pins:
+    button = DigitalIO(arcade_qt, button_pin)
+    button.direction = digitalio.Direction.INPUT
+    button.pull = digitalio.Pull.UP
+    buttons.append(button)
+
+# LED pins in order (1, 2, 3, 4)
+led_pins = (12, 13, 0, 1)
+leds = []
+for led_pin in led_pins:
+    led = PWMOut(arcade_qt, led_pin)
+    leds.append(led)
+
+
+def dispense(num):
+    print('start dispense: ', num)
+    leds[num].duty_cycle = 65535
+    #time.sleep(3)
+    # todo: get bartendro up
+    # todo: set the url of local bartendero, maybe to hostname? 
+    # todo: does urllib block? yes. So it is perfect
+    # todo: need to determine the pump.
+    # my 1,3,5,7 maps to dispensers 0,2,4,6
+    # 0,2 - good stuff
+    # 4,6 - You never expect the Malort
+    # map 0 and 1 to good and bad
+ 
+    good = 0 + 2*num 
+    bad = 4 + 2*num
+
+    pump = good
+    # the url is: 
+    # /dispenser/size in ml
+    # if button 0 is pushed, and then button 1, then  it appears
+    # that the right pump turns off prematurely when the left pump turns off.
+    # the led waits, but the pump stops.
+    # this mush be in bartendro.
+    apicall="http://ukr.local:8080//ws/dispense/%i/12" % pump
+    print('calling: ', apicall)
+    try:
+        with urllib.request.urlopen(apicall) as response:
+            rslt  = response.read()
+            print('bartendro returned: ', rslt)
+            time.sleep(2)
+    except urllib.error.URLError:
+        print('URLError trying to pour the shot. Is Bartendro running? num=', num)
+
+    leds[num].duty_cycle = 0 
+    print('end dispense: ', num)
+    flags[num]=0
+        
+    
+
+flags=[0,0]
+
+print('Ready for buttons...')
+while True:
+    for led_number, button in enumerate(buttons):
+        if led_number>1:
+            continue
+        if not button.value:
+            if flags[led_number] == 1:
+                #print('sorry already pressed')
+                pass
+            else:
+                flags[led_number] = 1
+                #print("Button pressed %i: value: %r" % (led_number, button.value))
+                x = Thread(target=dispense, args=(led_number,))
+                x.start()
+
+
